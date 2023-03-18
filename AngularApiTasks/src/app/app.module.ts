ba2af45402d7fff2b20cd37065931d8331e58044
Guillermo Riaño <guilledark17@gmail.com>
2023-03-18 11:04:26 -0600

    📦 Configuration:  Import Resources

--- AngularApiTasks/src/app/app.module.ts
+++ AngularApiTasks/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
